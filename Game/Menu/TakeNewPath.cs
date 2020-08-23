c8b4365ce9270734739bce92af322c9822d8b385
Reimer,Ben <Ben.Reimer@Cerner.com>
2020-08-23 13:51:00 -0400

    old code changes

--- Game/Menu/TakeNewPath.cs
+++ Game/Menu/TakeNewPath.cs
@@ -28,7 +28,7 @@ namespace Game.Menu
          //   newPath.PathList.Clear();
          //   newPath.PathList = updatedList;
             game.CurrentPath = newPath;
-            game.CurrentPath.Location = game.Character.CurrentLocation;
+            game.CurrentPath.Location = 0; // game.Character.CurrentLocation;
 
             //I can't set this to 0 because it would immediately flag that I was on a new path (the previous one) so I am going to set it to 1 for now
             game.Character.CurrentLocation = 0;
